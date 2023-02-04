8337b05b0c48ba0f286724bf0d8070164ed2d022
Kevin De Oliveira <contact@kevindeoliveira.com>
2023-02-04 01:51:25 -0500

    endpoint grpc

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using Grpc.Core;
 using IOT.Monitoring;
@@ -10,11 +12,11 @@ namespace GrpcClient
 {
     public class Client
     {
-        private Channel channel;
+        private Grpc.Core.Channel channel;
         private RemoteEndpoint.RemoteEndpointClient client;
         public Client(string address)
         {
-            channel = new Channel(address, ChannelCredentials.Insecure);
+            channel = new Grpc.Core.Channel(address, ChannelCredentials.Insecure);
             client = new RemoteEndpoint.RemoteEndpointClient(channel);
         }
 
@@ -43,12 +45,74 @@ namespace GrpcClient
             return result;
         }
 
+        public async Task<string> GetServerIdAsync()
+        {
+            var reply = await client.GetServerInfoAsync(new Empty());
+            return String.Format("{0}-{1}.{2}", reply.Major, reply.Minor, reply.Rpc);
+        }
+
         public List<Device> GetListOfDevices()
         {
             
             var response = client.ListDevices(new Empty());
 
             return response.Device.ToList();
+        }
+
+        public async Task<List<Device>> GetListOfDevicesAsync()
+        {
+
+            var response = await client.ListDevicesAsync(new Empty());
+
+            return response.Device.ToList();
+        }
+
+        public List<Packet> GetListOfPackets(int id)
+        {
+            var response = new List<Packet>();
+            var t = Task.Run(async () =>
+                {
+                    var stream = client.ReadPacket(new PacketRequest { 
+                        Id = id,
+                    });
+
+                    while (await stream.ResponseStream.MoveNext())
+                    {
+                        var cur = stream.ResponseStream.Current;
+                        if (cur == null)
+                            break;
+                        response.Add(cur);
+                    }
+
+                    
+                });
+
+            t.Wait();
+
+            return response;
+        }
+
+        public async Task ReadPackets(CancellationToken cancel, ChannelWriter<Packet> buffer)
+        {
+            var stream = client.ReadAll();
+            var sender = stream.ResponseStream;
+            var reader = stream.RequestStream;
+            _ = Task.Run(async () =>
+              {
+                  while (!cancel.IsCancellationRequested)
+                  {
+                      await reader.WriteAsync(new ReadAllOn { Active = true });
+                  }
+
+              });
+           
+            
+            while (await sender.MoveNext(cancel))
+            {
+                await buffer.WriteAsync(sender.Current);
+            }
+            await stream.RequestStream.CompleteAsync();
+            buffer.Complete();
         }
     }
     //class Program
--- IoT1/MainWindow.xaml.cs
+++ IoT1/MainWindow.xaml.cs
@@ -14,6 +14,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using GrpcClient;
+using System.ComponentModel;
+using System.Windows.Threading;
+using IOT.Monitoring;
+using System.Threading.Channels;
+using System.Threading;
 
 namespace IoT1
 {
@@ -22,7 +27,15 @@ namespace IoT1
     /// </summary>
     public partial class MainWindow : Window
     {
-        Client client;
+        struct connstatus
+        {
+            public string msg;
+            public bool active;
+        }
+        static Client client = null;
+        private static BackgroundWorker bgWorker;
+        private static CancellationTokenSource source = null;
+
         public MainWindow()
         {
            //Client _client = new Client(""); 
@@ -32,14 +45,238 @@ namespace IoT1
            // _client.GetListOfDevices();
 
                 InitializeComponent();
+
+            bgWorker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true,
+            };
+
+            
+            bgWorker.ProgressChanged += ProgressChanged;
+            bgWorker.RunWorkerCompleted += WorkCompleted;
             
         }
 
         private void Connect_Click(object sender, RoutedEventArgs e)
         {
-            client= new Client(""); //Add address
-            string result = client.GetServerId();
-            Test1.Text = result;
+
+            if(endpoint_addr.Text.Length == 0 || bgWorker.IsBusy)
+            {
+                return;
+            }
+
+            bgWorker.DoWork += ConnectToClient;
+
+            bgWorker.RunWorkerAsync(endpoint_addr.Text);
+            
+        }
+
+
+        //bgWorker tasks
+        void ConnectToClient(object sender, DoWorkEventArgs args)
+        {
+
+            Dispatcher.BeginInvoke(new Action(() => {
+                endpoint_conn_status.Content = "";
+                endpoint_addr.IsEnabled = false;
+                Connect.IsEnabled = false;
+            }), DispatcherPriority.Background);
+
+            var addr = (string)args.Argument;
+            addr += ":6501";
+            client = new Client(addr);
+            connstatus result = new connstatus { };
+            try
+            {
+                var res = client.GetServerId();
+
+                result.msg = res;
+                result.active = true;
+            }
+            catch (Exception e)
+            {
+                result.msg = e.Message;
+                result.active = false;
+            }
+
+            args.Result = result;
+
+        }
+
+        void ReadDevicePackets(object sender, DoWorkEventArgs args)
+        {
+
+            if (client == null)
+                return;
+            
+            var id = (int)args.Argument;
+
+            List<Packet> response = new List<Packet>();
+
+            try
+            {
+                response = client.GetListOfPackets(id);
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            args.Result = response;
+
+        }
+
+        void ReadAllPackets(object sender, DoWorkEventArgs args)
+        {
+
+            if (client == null)
+                return;
+            Channel<Packet> buffer = Channel.CreateUnbounded<Packet>();
+
+            source = new CancellationTokenSource();
+
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    await client.ReadPackets(source.Token, buffer.Writer);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally { 
+                    source.Cancel(false);
+                    buffer.Writer.TryComplete();
+                }
+            });
+                        
+            args.Result = buffer.Reader;
+        }
+
+        static void ProgressChanged(object sender, ProgressChangedEventArgs args)
+        {
+
+        }
+
+        void WorkCompleted(object sender, RunWorkerCompletedEventArgs args)
+        {
+            if (args.Result == null)
+                return;
+
+            if ( typeof(connstatus) == args.Result.GetType())
+            {
+                var res = (connstatus)args.Result;
+                Dispatcher.BeginInvoke(new Action(() => {
+
+                    if (res.active)
+                    {
+                        var bc = new BrushConverter();
+                        endpoint_conn_status.Content = "Connected -- Server v" + res.msg;
+                        endpoint_conn_status.Background = (Brush)bc.ConvertFrom("#00FF00");
+
+                    }
+                    else
+                    {
+                        endpoint_addr.IsEnabled = true;
+                        endpoint_conn_status.Content = res.msg;
+                        Connect.IsEnabled = true;
+                    }
+                }), DispatcherPriority.Background);
+                bgWorker.DoWork -= ConnectToClient;
+            }
+            else if(typeof(List<Packet>) == args.Result.GetType())
+            {
+                var res = (List<Packet>)args.Result;
+
+                TextBox[] textBoxes = { heart_rate, co_2, temp, oxygen };
+                
+                Dispatcher.BeginInvoke(new Action(() => {
+
+                    if (res.Count == 0)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        for(int i = 0; i < res.Count(); i++)
+                        {
+                            textBoxes[res[i].Id % textBoxes.Length].Text = res[i].Data.ToStringUtf8();
+                        }                        
+                    }
+                }), DispatcherPriority.Background);
+
+            }
+            else
+            // if(typeof(ChannelReader<Packet>) == args.Result.GetType())
+            {
+                var buf = (ChannelReader<Packet>)args.Result;
+
+                Dispatcher.BeginInvoke(new Action(async() => {
+                    TextBox[] textBoxes = { heart_rate, co_2, temp, oxygen };
+                    Ellipse[] ellipses = { heart_rate_st, co_2_st, temp_st, oxygen_st };
+                    var bc = new BrushConverter();
+
+
+                    int i = 0;
+                    for (int j = 0; j < textBoxes.Length; j++)
+                    {
+                        textBoxes[j].IsEnabled = false;
+                        ellipses[j].Fill = (Brush)bc.ConvertFrom("#00FF00");
+                    }
+                    while (await buf.WaitToReadAsync())
+                    {
+                        var data = await buf.ReadAsync();
+                        textBoxes[data.Id % textBoxes.Length].Text = data.Data.ToStringUtf8();
+                    }
+                    
+                    for (int j = 0; j < textBoxes.Length; j++)
+                    {
+                        textBoxes[j].IsEnabled = true;
+                        ellipses[j].Fill = (Brush)bc.ConvertFrom("#FF0000");
+                    }
+                    StartBtn.IsEnabled = true;
+                    StopBtn.IsEnabled = false;
+
+                }), DispatcherPriority.Normal);
+
+            }
+
+
+
+
+
+        }
+
+        private void endpoint_addr_MouseEnter(object sender, MouseEventArgs e)
+        {
+            endpoint_addr.Text = "";
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (endpoint_addr.IsEnabled || bgWorker.IsBusy)
+            {
+                return;
+            }
+            Dispatcher.BeginInvoke(new Action(() => {
+                StartBtn.IsEnabled = false;
+                StopBtn.IsEnabled = true;
+            }), DispatcherPriority.Background);
+
+            bgWorker.DoWork += ReadAllPackets;
+
+            bgWorker.RunWorkerAsync(1);
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (source == null)
+                return;
+            source.Cancel();
+            bgWorker.DoWork -= ReadAllPackets;
         }
     }
 }
